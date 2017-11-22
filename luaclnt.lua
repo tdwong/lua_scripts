daa1ecba3be868d40df4ab9008f53bd9834c768d
tdwong <td.wong@tp-link.com>
2017-11-22 14:11:35 -0800

    added debug printout

--- luaclnt.lua
+++ luaclnt.lua
@@ -7,19 +7,26 @@
 --	v2	- send and receive reply
 --
 
+SOCKET=os.getenv("SOCKET") or "/tmp/uds_socket"
+if #arg>0 then SOCKET=arg[1] end
+
 socket=require"socket"
 unix=require"socket.unix"
 c=unix()
-assert(c:connect("/tmp/zz"))
+assert(c:connect(SOCKET))
 
-	local outString = "string-goes-to-server\n"
+	print( "client connected to "..SOCKET )
+
+	local outString = "string-goes-to-server"
 
 while 1 do
-	res, err, nsent = c:send( outString )
+	-- NOTE: append a '\n' so that receiving end can do socket:receive("*l")
+	res, err, nsent = c:send( outString..'\n' )
 	print( "res="..tostring(res)..", err="..tostring(err)..", nsent="..tostring(nsent) )
 	if res then
 		print( "sent ..(" ..outString.. ")" )
 		res, err, elapsed = c:receive("*l")
+		if res then print( "server reply: (" .. res .. ")" ) end
 	else
 		print( "failed to send... punt..." )
 		break
--- luaserv.lua
+++ luaserv.lua
@@ -7,25 +7,37 @@
 --	v2	- receive and send reply
 --
 
+SOCKET=os.getenv("SOCKET") or "/tmp/uds_socket"
+if #arg>0 then SOCKET=arg[1] end
+
 require "socket"
 unix=require"socket.unix"
 s=unix()
 
 	require "nixio"
-	if nixio.fs.access("/tmp/zz") then
-		nixio.fs.remove("/tmp/zz")
+	if nixio.fs.access(SOCKET) then
+		nixio.fs.remove(SOCKET)
 	end
 
-assert(s:bind("/tmp/zz"))
+assert(s:bind(SOCKET))
 s:listen(5)
+
+	print( "listend on "..SOCKET )
+
 c=s:accept()
+
+	print( "client connected "..tostring(c) )
+
+	local replyStr = "reply"
 
 while 1 do
 	res, err, elapsed = c:receive("*l")
 	if res then
 		print( res )
 		-- send reply
-		res, err, nsent = c:send( "reply\n" )
+		-- NOTE: append a '\n' so that receiving end can do socket:receive("*l")
+		res, err, nsent = c:send( replyStr..'\n' )
+		if res then print( "reply sent ..(" ..replyStr.. ")" ) end
 	else
 		print( "failed to receive... punt..." )
 		break
