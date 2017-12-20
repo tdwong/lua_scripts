5f25cae443532892f45aced3b1a23a411227f758
tdwong <tdwong12@gmail.com>
2017-12-19 17:27:11 -0800

    enhancement on both client & server
    client : v4- allow customized initial string & add simple help
    server L v4- add timeout (in listen and receive) & receive mode

--- lua_unix_domain_socket/lua_unix_domain_clnt.lua
+++ lua_unix_domain_socket/lua_unix_domain_clnt.lua
@@ -6,19 +6,33 @@
 --	v1	- send only
 --	v2	- send and receive reply
 --	v3	- interoperability tested (with socat & udmStrServ)
+--	v4	- allow customized initial string & add simple help
 --
+
+-- simple command line help
+local usage = "usage: "..arg[0].." [unix-domain-socket-path] [initial-outgoing-string]"
+if #arg>0 then
+	for i=1,#arg do
+		if arg[i]=='-h' or arg[i]=='--help' then print( usage ) ; return end
+	end
+end
+
 
 SOCKET=os.getenv("SOCKET") or "/tmp/uds_socket"
 if #arg>0 then SOCKET=arg[1] end
 
+local outString = "string-goes-to-server"
+if #arg>1 then outString=arg[2] end
+
 socket=require"socket"
 unix=require"socket.unix"
 c=unix()
-assert(c:connect(SOCKET))
+assert(c:connect(SOCKET))		-- assert() never returns
 
 	print( "client connected to "..SOCKET )
 
-	local outString = "string-goes-to-server"
+local no_recv=os.getenv('NO_RECEIVE')
+if no_recv==nil then no_recv=false else no_recv=true end
 
 while 1 do
 	-- NOTE: append a '\n' so that receiving end can do socket:receive("*l")
@@ -26,8 +40,10 @@ while 1 do
 	print( "res="..tostring(res)..", err="..tostring(err)..", nsent="..tostring(nsent) )
 	if res then
 		print( "sent ..(" ..outString.. ")" )
-		res, err, elapsed = c:receive("*l")
-		if res then print( "server reply: (" .. res .. ")" ) end
+		if no_recv==false then
+			res, err, elapsed = c:receive("*l")
+			if res then print( "server reply: (" .. res .. ")" ) end
+		end
 	else
 		print( "failed to send... punt..." )
 		break
@@ -36,7 +52,8 @@ while 1 do
 	--socket.sleep(2)
 	io.write( "enter data to be sent: " )
 	outString = io.read()
-	if #outString==1 and outString=='.' then break end
+		-- print( 'outString: '..outString..', #outString: '..tostring(#outString) )
+	if #outString==1 and (outString=='.' or outString=='q') then break end
 end
 
 c:close()
--- lua_unix_domain_socket/lua_unix_domain_serv.lua
+++ lua_unix_domain_socket/lua_unix_domain_serv.lua
@@ -6,12 +6,13 @@
 --	v1	- receive only
 --	v2	- receive and send reply
 --	v3	- interoperability tested (with socat & udmStrClnt)
+--	v4	- add timeout (in listen and receive) & receive mode
 --
 
 SOCKET=os.getenv("SOCKET") or "/tmp/uds_socket"
 if #arg>0 then SOCKET=arg[1] end
 
-require "socket"
+socket=require "socket"
 unix=require"socket.unix"
 s=unix()
 
@@ -20,17 +21,47 @@ s=unix()
 		nixio.fs.remove(SOCKET)
 	end
 
-assert(s:bind(SOCKET))
+assert(s:bind(SOCKET))		-- assert() never returns
 s:listen(5)
 
 	print( "listend on "..SOCKET )
 
-c=s:accept()
+	-- --
+	local acceptTimeout=os.getenv('ACCEPT_TIMEOUT')
+	if acceptTimeout then acceptTimeout = tonumber(acceptTimeout) end
+	if acceptTimeout then s:settimeout(acceptTimeout) end
+	-- --
+
+while 1 do
+	--
+	print( "accepting @ "..os.time() )
+	c, err =s:accept()
+	
+	if c then break
+	else
+		if err=='timeout' then
+			print( "accept: timeout @ "..os.time().. ", after "..tostring(acceptTimeout).." seconds, continue waiting..." )
+		else
+			print( "accept: error: "..err )
+			return
+		end
+	end
+	--
+end	-- while
+
+	-- --
+	local receiveTimeout=os.getenv('RECEIVE_TIMEOUT')
+	if receiveTimeout then receiveTimeout = tonumber(receiveTimeout) end
+	if receiveTimeout then c:settimeout(receiveTimeout) end
+	-- --
 
 	print( "client connected "..tostring(c) )
 
+local receive_mode=os.getenv('RECEIVE_MODE') or '*l'
 while 1 do
-	res, err, elapsed = c:receive("*l")
+	--
+	print( "receiving @ "..os.time() )
+	res, err, nbytes = c:receive(receive_mode)
 	if res then
 		print( res )
 		-- send reply
@@ -39,8 +70,21 @@ while 1 do
 		res, err, nsent = c:send( replyStr..'\n' )
 		if res then print( "reply sent ..(" ..replyStr.. ")" ) end
 	else
-		print( "err="..tostring(err)..", elapsed="..tostring(elapsed) )
-		print( "receive nothing... punt..." )
-		break
+		if err~='timeout' then
+			print( "err="..tostring(err) )
+			print( "receive nothing... punt..." )
+			c:close()
+			break
+		else
+			print( "receive: timeout @ "..os.time().. ", after "..tostring(receiveTimeout).." seconds, abort receiving..." )
+			--
+			-- -- alternatively, close the client socket and goes back to listen
+			--
+			-- c:close()
+			-- break
+		end
 	end
 end
+
+s:close()
+
